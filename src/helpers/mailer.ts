142bf2dafd8946f5e8c8eed598e2bf469b579d94
anagh-azad <anagh.azad.ug20@nsut.ac.in>
2024-05-21 17:16:44 +0530

    add userModel mailer and signup route

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,53 @@
+import User from "@/models/user.model";
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000
+      });
+    }
+
+    const transport = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D"
+      }
+    });
+
+    const mailOptions = {
+      from: "anaghazad62@gmail.com",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+            or copy and paste the link below in your browser. <br> ${
+              process.env.DOMAIN
+            }/verifyemail?token=${hashedToken}
+            </p>`
+    };
+
+    const mailResponse = await transport.sendMail(mailOptions);
+
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
